ceec9a5d0fa1a953c07b0f138f840007cdea61be
Roni Kallio <roni@kallio.app>
2021-01-26 13:58:16 +0200

    Service for database table 'item'

---
+++ api/src/services/item.service.js
@@ -0,0 +1,43 @@
+import sequelize from '../sequelize.init.js';
+const { Item } = sequelize.models;
+
+export const findAll = () => {
+    return Item.findAll();
+}
+
+export const findByKey = (key) => {
+    return Item.findByPk(key, {
+        rejectOnEmpty: true
+    });
+}
+
+export const create = (data) => {
+    return Item.create(data);
+}
+
+export const updateByKey = (key, data) => {
+    const updated = sequelize.transaction(async t => {
+        const item = await Item.findByPk(key, {
+            transaction: t,
+            rejectOnEmpty: true
+        });
+        const updated = item.update(data, {
+            transaction: t
+        });
+        return updated;
+    });
+    return updated;
+}
+
+export const deleteByKey = (key) => {
+    const deleted = sequelize.transaction(async t => {
+        const item = await Item.findByPk(key, {
+            transaction: t,
+            rejectOnEmpty: t
+        });
+        return item.destroy({
+            transaction: t
+        });
+    })
+    return deleted;
+}
