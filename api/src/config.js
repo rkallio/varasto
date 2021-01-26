3cc4130ed17d0a5c48bbf3b97a4d7c4ec58ac1c2
Roni Kallio <roni@kallio.app>
2021-01-26 14:09:29 +0200

    Environment variable manager

---
+++ api/src/config.js
@@ -0,0 +1,39 @@
+import { format } from 'util';
+
+const rules = {
+    INVENTORY_DB_DIALECT: {
+        coercer: String,
+    },
+    INVENTORY_DB_STORAGE: {
+        coercer: String,
+    }
+};
+
+const env = process.env;
+
+const mismatch = Object.keys(rules)
+      .map(ruleName => {
+          if(Object.keys(env).some(keyName => keyName === ruleName)) {
+              return [true, ruleName];
+          } else {
+              return [false, ruleName];
+          }
+      })
+      .filter(([exist, _]) => !exist)
+      .map(([_, name]) => name);
+
+if(mismatch.length > 0) {
+    throw Error(format('Missing properties: %s', mismatch));
+};
+
+export default Object.entries(rules)
+    .map(([key, props]) => {
+        const o = Object.entries(env)
+              .find(([k, _]) => key === k);
+        return [props.rename ? props.rename : key,
+                props.coercer ? props.coercer(o[1]) : o[1]];
+    })
+    .reduce((prev, [key, value]) => {
+        prev[key] = value;
+        return prev;
+    }, {});
