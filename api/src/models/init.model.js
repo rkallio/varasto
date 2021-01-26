1ac4070c2e24c515d6a74b284fa8a11afbf525d9
Roni Kallio <roni@kallio.app>
2021-01-26 13:56:50 +0200

    Database table 'item'

---
+++ api/src/models/init.model.js
@@ -0,0 +1,2 @@
+import sequelize from '../sequelize.init.js';
+import './item.model.js';
---
+++ api/src/models/item.model.js
@@ -0,0 +1,38 @@
+import Sequelize from 'sequelize';
+const { Model } = Sequelize;
+import sequelize from '../sequelize.init.js';
+
+class Item extends Model { };
+
+Item.init({
+    name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: 'name-location-composite',
+        validate: {
+            len: [3, 20]
+        },
+    },
+    location: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: 'name-location-composite',
+        validate: {
+            len: [3, 20]
+        }
+    },
+    quantity: {
+        type: Sequelize.DataTypes.REAL,
+        defaultValue: 0,
+        allowNull: false,
+        validate: {
+            min: 0
+        }
+    }
+}, {
+    sequelize,
+    underscored: true,
+    modelName: 'Item'
+});
+
+export default Item;
