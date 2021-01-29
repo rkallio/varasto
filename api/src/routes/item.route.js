f357f39aff32472870ae20f4de84de557238564b
Roni Kallio <roni@kallio.app>
2021-01-29 16:08:17 +0200

    Get item with id route

--- api/src/routes/item.route.js
+++ api/src/routes/item.route.js
@@ -8,6 +8,11 @@ router.get('/', async (request, response) => {
     return response.send(await itemService.findAll());
 });
 
+router.get('/:id', async (request, response) => {
+    const id = request.params.id;
+    return response.send(await itemService.findByKey(id));
+});
+
 router.post('/', async (request, response) => {
     return response.send(await itemService.create(request.body));
 })
