b6578b8fa70b64c4aae40f27eec96f3c8e97c940
Roni Kallio <roni@kallio.app>
2021-01-29 21:47:55 +0200

    Handle sequelize empty result error as a 404

--- api/src/index.js
+++ api/src/index.js
@@ -8,6 +8,7 @@ import config from './config.js';
 const app = express();
 const server = http.createServer(app);
 
+import Sequelize from 'sequelize';
 import sequelize from './sequelize.init.js';
 import './models/init.model.js';
 await sequelize.sync();
@@ -19,6 +20,19 @@ import routes from './routes/init.route.js';
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(routes);
+
+// sequelize handlers
+app.use((error, request, response, next) => {
+    if(error instanceof Sequelize.EmptyResultError) {
+        return response.status(404).json({
+            error: 'Not Found',
+            message: 'Resource not found',
+            context: null
+        });
+    }
+    return next(error);
+})
+
 app.use((request, response) => {
     response.status(404).json({
         error: 'Not Found',
