e23572179e9c9279c62f4d93907d0b4b0d1df975
Roni Kallio <roni@kallio.app>
2021-02-09 13:43:54 +0200

    Web: rename redux.js to store.js

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './redux.js';
+import store from './store.js';
 import App from './app.jsx';
 import { Provider } from 'react-redux';
 
