84dabd8e474bf312cc3b4ae93652b8be7a25ca8d
Roni Kallio <roni@kallio.app>
2021-02-09 15:37:31 +0200

    Web: display login page under /login

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -1,3 +1,17 @@
-import React from 'react';
-const App = () => <h1>Hello, world!</h1>;
-export default App;
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+
+import { Switch, Route } from 'react-router-dom';
+
+import LoginPage from './login-page.jsx';
+// import ItemPage from './item-page.jsx';
+
+export default () => {
+    return (
+        <Switch>
+            <Route path="/login">
+                <LoginPage />
+            </Route>
+        </Switch>
+    );
+}
