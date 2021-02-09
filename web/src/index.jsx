87e8bb86e528eb41a05a5629aea292d931fe8c8e
Roni Kallio <roni@kallio.app>
2021-02-09 13:14:46 +0200

    Web: Introduce redux

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './redux.js';
 import App from './app.jsx';
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
