e1334d5cbd4b5455c6ed72f4583811042c29dd86
Roni Kallio <roni@kallio.app>
2021-02-14 15:28:17 +0200

    Web: conditional socket connection

--- web/src/socket-middleware.js
+++ web/src/socket-middleware.js
@@ -1,29 +1,61 @@
 import { io } from 'socket.io-client';
 import { actions } from './item.redux.js';
+import {
+    authenticate,
+    selector as tokenSelector
+} from './auth.redux.js';
+
 
 export default () => {
     return store  => {
-        const socket = io('/items', {
-            auth: {
-                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6InJvbmkiLCJpYXQiOjE2MTI4OTM3MjR9.KgbBJtx8KOzxY2FdOFLgzkgoU7iNCY-Z_lP6CVGVSiU'
+        let token = tokenSelector(store.getState());
+        let socket;
+
+        if(token) {
+            socket = io('/items', {
+                auth: { token }
+            });
+        }
+
+        if(socket) {
+            socket.on('post', data => {
+                store.dispatch(actions.addOne(data));
+            });
+
+            socket.on('patch', data => {
+                store.dispatch(actions.updateOne({
+                    id: data.id,
+                    changes: data
+                }));
+            });
+
+            socket.on('delete', data => {
+                store.dispatch(actions.removeOne(data.id));
+            });
+        }
+
+        return next => action => {
+            if(action.type && action.type === authenticate.fulfilled.type) {
+                token = action.payload;
+                socket = io('/items', {
+                    auth: { token }
+                });
+                socket.on('post', data => {
+                    store.dispatch(actions.addOne(data));
+                });
+
+                socket.on('patch', data => {
+                    store.dispatch(actions.updateOne({
+                        id: data.id,
+                        changes: data
+                    }));
+                });
+
+                socket.on('delete', data => {
+                    store.dispatch(actions.removeOne(data.id));
+                });
             }
-        });
-
-        socket.on('post', data => {
-            store.dispatch(actions.addOne(data));
-        });
-
-        socket.on('patch', data => {
-            store.dispatch(actions.updateOne({
-                id: data.id,
-                changes: data
-            }));
-        });
-
-        socket.on('delete', data => {
-            store.dispatch(actions.removeOne(data.id));
-        });
-
-        return next => action => next(action);
+            return next(action);
+        }
     }
 }
