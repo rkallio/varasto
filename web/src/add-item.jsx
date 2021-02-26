d04f93a11ece1f899d4e73db43b1187f634b633e
Roni Kallio <roni@kallio.app>
2021-02-26 11:14:59 +0200

    Web: Extract item form to a separate component

--- web/src/add-item.jsx
+++ web/src/add-item.jsx
@@ -1,66 +1,30 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { postItem } from './item.redux.js';
 import { actions } from './modal.redux.js';
-import * as Forms from './forms.jsx';
-import * as Items from './items.jsx';
 import Container from './container.jsx';
-import If from './if.jsx';
-import useItemForm from './use-item-form.js';
+import ItemForm from './item-form.jsx';
+import { Button } from './forms.jsx';
 
 export default AddItem = () => {
     const dispatch = useDispatch();
-    const { values, handleChange, onSubmit } = useItemForm(
-        {},
-        postItem
-    );
 
     return (
         <Container>
-            <form onSubmit={onSubmit}>
-                <Items.NameInput
-                    value={values.name}
-                    onChange={handleChange}
-                    required
-                />
-                <Items.LocationInput
-                    value={values.location}
-                    onChange={handleChange}
-                    required
-                />
-                <Items.CurrentQuantityInput
-                    value={values.currentQuantity}
-                    onChange={handleChange}
-                    required
-                />
-                <If cond={values.measure !== '%'}>
-                    <Items.TargetQuantityInput
-                        value={values.targetQuantity}
-                        onChange={handleChange}
-                        required
-                    />
-                </If>
-                <Items.MeasureInput
-                    value={values.measure}
-                    onChange={handleChange}
-                />
-                <Forms.FieldContainer>
-                    <Forms.ButtonGroup>
-                        <Forms.Button type="submit">
-                            Submit
-                        </Forms.Button>
-                        <Forms.Button
-                            type="button"
-                            onClick={() =>
-                                dispatch(actions.closeModal())
-                            }
-                        >
-                            Close
-                        </Forms.Button>
-                    </Forms.ButtonGroup>
-                </Forms.FieldContainer>
-            </form>
+            <ItemForm
+                method={postItem}
+                actionButtons={[
+                    <Button
+                        key="close"
+                        type="button"
+                        onClick={() => {
+                            dispatch(actions.closeModal());
+                        }}
+                    >
+                        Close
+                    </Button>,
+                ]}
+            />
         </Container>
     );
 };
---
+++ web/src/item-form.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import If from './if.jsx';
+import useItemForm from './use-item-form.js';
+import { itemSelector } from './item.redux.js';
+import * as Items from './items.jsx';
+import * as Forms from './forms.jsx';
+
+const ItemForm = (props) => {
+    const item = useSelector((state) =>
+        itemSelector.selectById(state, props.id)
+    );
+
+    const { values, handleChange, onSubmit } = useItemForm(
+        item,
+        props.method
+    );
+
+    return (
+        <form onSubmit={onSubmit}>
+            <Items.NameInput
+                value={values.name}
+                onChange={handleChange}
+                required
+            />
+            <Items.LocationInput
+                value={values.location}
+                onChange={handleChange}
+                required
+            />
+            <Items.CurrentQuantityInput
+                value={values.currentQuantity}
+                onChange={handleChange}
+                required
+            />
+            <If cond={values.measure !== '%'}>
+                <Items.TargetQuantityInput
+                    value={values.targetQuantity}
+                    onChange={handleChange}
+                    required
+                />
+            </If>
+            <Items.MeasureInput
+                value={values.measure}
+                onChange={handleChange}
+            />
+            <Forms.FieldContainer>
+                <Forms.ButtonGroup>
+                    <Forms.Button type="submit">Submit</Forms.Button>
+                    {props.actionButtons}
+                </Forms.ButtonGroup>
+            </Forms.FieldContainer>
+        </form>
+    );
+};
+
+ItemForm.propTypes = {
+    id: PropTypes.number,
+    method: PropTypes.func.isRequired,
+    actionButtons: PropTypes.arrayOf(PropTypes.element),
+};
+
+export default ItemForm;
