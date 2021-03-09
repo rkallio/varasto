32d3c4591e4584394f6a9c23920a789fbbbf5631
Roni Kallio <roni@kallio.app>
2021-03-09 17:20:12 +0200

    API: User model tests

---
+++ api/test/models/user.test.js
@@ -0,0 +1,88 @@
+const { describe, it } = require('mocha');
+const chai = require('chai');
+const { assert } = chai;
+const proxyquire = require('proxyquire').noCallThru();
+const sinon = require('sinon');
+
+const ignore = () => {};
+
+describe('user model', () => {
+  const REQUIRE_PATH = '../../src/models/user.model.js';
+
+  describe('test credentials against database', () => {
+    describe('username does not match', () => {
+      it('.ok is false @unit', async () => {
+        const find = sinon.fake.returns(null);
+
+        class Model {}
+        Model.findOne = find;
+
+        const { test } = proxyquire(REQUIRE_PATH, {
+          sequelize: { Model },
+          bcrypt: {},
+          lodash: { get: ignore },
+          './model-utils.js': {},
+        });
+
+        const result = await test();
+
+        assert.propertyVal(result, 'ok', false);
+      });
+    });
+
+    describe('username matches', () => {
+      describe('password comparison succeeds', () => {
+        it('.ok is true', async () => {
+          const compare = sinon.fake.resolves(true);
+          class Model {}
+          Model.findOne = () => ({ password: {} });
+
+          const { test } = proxyquire(REQUIRE_PATH, {
+            sequelize: { Model },
+            bcrypt: { compare },
+            lodash: { get: ignore },
+            './model-utils.js': {},
+          });
+
+          const result = await test();
+          assert.propertyVal(result, 'ok', true);
+        });
+
+        it('.user is matched user', async () => {
+          const compare = sinon.fake.resolves(true);
+          const expected = { password: {} };
+          class Model {}
+          Model.findOne = () => expected;
+
+          const { test } = proxyquire(REQUIRE_PATH, {
+            sequelize: { Model },
+            bcrypt: { compare },
+            lodash: { get: ignore },
+            './model-utils.js': {},
+          });
+
+          const result = await test();
+          assert.propertyVal(result, 'user', expected);
+        });
+      });
+
+      describe('password comparison fails', async () => {
+        it('.ok is false', async () => {
+          const compare = sinon.fake.resolves(false);
+          class Model {}
+          Model.findOne = () => ({ password: {} });
+
+          const { test } = proxyquire(REQUIRE_PATH, {
+            sequelize: { Model },
+            bcrypt: { compare },
+            lodash: { get: ignore },
+            './model-utils.js': {},
+          });
+
+          const result = await test();
+          assert.propertyVal(result, 'ok', false);
+        });
+      });
+    });
+  });
+});
